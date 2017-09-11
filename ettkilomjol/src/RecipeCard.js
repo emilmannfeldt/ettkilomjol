535360088040099f846d13469fd51fd0941ca494
Emil Mannfeldt <emil.mannfeldt@gmail.com>
2017-09-11 18:00:46 +0000

    fail try to implement react routing. added Baketime to recipecards

--- ettkilomjol/src/RecipeCard.js
+++ ettkilomjol/src/RecipeCard.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import './App.css'; //fixa en egen css för varje komponent?
 import IngredientTag from './IngredientTag';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
-
+import BakeTime from './BakeTime';
 
 
 
 class RecipeCard extends Component {
-      constructor(props) {
+  constructor(props) {
     super(props);
   }
 
   styles = {
     recipeCard: {
       margin: 4,
-     
+
     },
     wrapper: {
 
     },
-    title:{
+    title: {
       fontSize: 14,
-      lineHeight:1
+      lineHeight: 1
     }
   };
   render() {
@@ -30,17 +30,15 @@ class RecipeCard extends Component {
     let matchedIngredients = [];
     let missingIngredients = [];
     for (let property in this.props.recipe.ingredients) {
-        if (this.props.recipe.ingredients.hasOwnProperty(property)) {
-            if(this.props.filter.ingredients.indexOf(property)>-1){
-                matchedIngredients.push(property);
-            }else{
-                missingIngredients.push(property);
-            }
+      if (this.props.recipe.ingredients.hasOwnProperty(property)) {
+        if (this.props.filter.ingredients.indexOf(property) > -1) {
+          matchedIngredients.push(property);
         }
+        else {
+          missingIngredients.push(property);
+        }
+      }
     }
-
-
-
 
 
 
@@ -49,19 +47,19 @@ class RecipeCard extends Component {
     //skapa recept kan också vara routing/modalt fönster/formuilär fälls ut.
     //mer som ska till recipecard: betyg, tidsåtgång, lägg till i favoriter (koket.se hjärta). 
     //
-    return ( <div className="col-lg-4 col-md-4 col-sm-4 col-xs-6 list-item" style={this.styles.wrapper}> <Card className="recipecard-content" style={this.styles.recipeCard}>
+    return (<div className="col-lg-4 col-md-4 col-sm-4 col-xs-6 list-item" style={this.styles.wrapper}> <Card className="recipecard-content" style={this.styles.recipeCard}>
                 <div className="recipe-card-image">
-                <img src={this.props.recipe.image}/>
+                  <img src={this.props.recipe.image}/>
                 </div>
                 <CardText className="recipe-card-info">
                 <div className="recipecard-title">{this.props.recipe.title} </div>
                 
           <IngredientTag
                   matchedIngredients={matchedIngredients} missingIngredients={missingIngredients}/>
+                <BakeTime time={this.props.recipe.time}/>
                 </CardText>
               </Card>
-              </div>
-);
+              </div>);
   }
 }
 export default RecipeCard;
