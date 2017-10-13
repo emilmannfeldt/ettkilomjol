32925f811dcde038e66043e7e67f0cc5a428fc77
emil <emilmannfeldt93@gmail.com>
2017-10-14 00:17:17 +0200

    påbörjat scrape script

--- ettkilomjol/src/night2.js
+++ ettkilomjol/src/night2.js
@@ -1,31 +1,14 @@
 var Nightmare = require('nightmare');
-var nightmare = Nightmare({ openDevTools: true, show: true })
-// var Xray = require('x-ray');
-// var x = Xray();
+var nightmare = Nightmare({ openDevTools: true, show: false })
+var fs = require('fs');
 
 nightmare
-.goto('https://www.koket.se/mat/hogtid/kraftskiva')
-.wait(2500)
-.click('a[rel="next"]')
+.goto('https://www.koket.se/mat/typ-av-maltid/vardagsmiddag')
 .wait(2500)
 .evaluate(function(){
   //using `Array.from` as the DOMList is not an array, but an array-like, sort of like `arguments`
   //planning on using `Array.map()` in a moment
-  function visaFlerExists(){
-    $(".next.searchPageButton").each(function(){
-      if($(this).css('display') != 'none'){
-        return true;
-      }
 
-    })
-    return false;
-  }
-
-  while(visaFlerExists()) {
-    nightmare.click("a[rel='next']");
-    nightmare.wait(2000);
-  }
-  nightmare.wait(10000);
   return Array.from(
     //give me all of the elements where the href contains 'Property.aspx'
     document.querySelectorAll('article.list-item.recipe .info .top h2 a'))
@@ -43,12 +26,61 @@ nightmare
   return hrefs.reduce(function(accumulator, href){
     //return the accumulated promise results, followed by...
     return accumulator.then(function(results){
-      return nightmare
-        //click on the href
-        .goto(href)
+      return nightmare.goto(href)
         //get the html
         .evaluate(function(){
-          return document.querySelector('.recipe-content-wrapper h1').innerHTML;
+          if(document.querySelector('.recipe-column-wrapper .endorsed-banner')){
+            return;
+          }
+          let recipe = {};
+          //title
+          recipe.title = document.querySelector('.recipe-content-wrapper h1').innerHTML;
+          //tags
+          let tags = {};
+          $('.category-touch-scroll .btn.green-btn.category').each(function(){
+              let t = $(this).text().split('/');
+              for(let i=0; i<t.length; i++){
+                tags[t[i]] = true;
+              }
+          })
+          recipe.tags = tags;
+          //source
+          recipe.source = window.location.href;
+          //rating
+          recipe.rating = document.querySelector('.recipe-content-wrapper .rating-container.rating').getAttribute("data-setrating");
+          //votes
+          recipe.votes = document.querySelector('.recipe-content-wrapper .rating-container.rating span.text').innerHTML.split(" ")[0];
+          //author
+          if(document.querySelector('.recipe-content-wrapper .author-chef a')){
+            recipe.author = document.querySelector('.recipe-content-wrapper .author-chef a').innerHTML;
+          }
+          //createdFor
+          if(document.querySelector('.recipe-content-wrapper .author-source a')){
+            recipe.createdFor = document.querySelector('.recipe-content-wrapper .author-source a').innerHTML;
+          }
+          //portions  "4småpotioner" trattkantarellsoppan. replace alla bokstäver med ""?
+          if(document.querySelector('.recipe-content-wrapper .recipe-info.portions span.amount')){
+            recipe.portions = document.querySelector('.recipe-content-wrapper .recipe-info.portions span.amount').innerHTML.replace(/(\r\n|\n|\r| )/gm,"").trim();
+          }
+          //created
+          if(document.querySelector('.recipe-content-wrapper .description meta[itemprop="datePublished"]')){
+            recipe.created = document.querySelector('.recipe-content-wrapper .description meta[itemprop="datePublished"]').getAttribute("content");
+          }
+          //description
+          if(document.querySelector('.recipe-content-wrapper .recipe-article .description p')){
+            recipe.description = $('.recipe-content-wrapper .recipe-article .description p:first-child').text().replace(/(\r\n|\n|\r|)/gm,"");
+          }
+
+          //imageurl
+          //diffuculty (om det inte finns så låt kolla metoden i datachange för hur det ska räknas, testa den.)
+          //time
+          //ingredients
+            //amount
+            //unit
+            //food
+
+
+          return recipe;
         })
         //add the result to the results
         .then(function(html){
@@ -70,6 +102,14 @@ nightmare
 .then(function (resultArr) {
   //if I haven't made a mistake above with the `Array.reduce`, `resultArr` should now contain all of your links' results
   console.log('resultArr', resultArr);
-  // x(resultArr[1], 'body@html') //output listing page html
-  //   .write('results.json');
+  console.log(resultArr.length);
+
+
+    fs.writeFile("C:/react/test.txt", JSON.stringify(resultArr), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
 });
--- ettkilomjol/src/nightmareRun.js
+++ ettkilomjol/src/nightmareRun.js
@@ -1,75 +1,13 @@
-var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: true })
+    var fs = require('fs');
 
-//denna fil är specifik för koket.se
-//sno kod för att spara ner recepten från datachange.js
-//glöm inte spara foods (uses), units
+fs.readFile('C:/react/test.txt', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var result = JSON.parse(data);
+  console.log(result);
 
+  console.log(result[0].tags.snabbt);
+    console.log(result.length);
 
-
-nightmare
-  .goto('https://www.koket.se/mat/typ-av-maltid/vardagsmiddag')
-  .wait(2000)
-  .evaluate(function () {
-    let recipesScraped = 0;
-    let recipesources = [];
-
-      let recipesLoop = [];
-      let visibleRecipeCards = $('article.list-item.recipe');
-      $(visibleRecipeCards).each(function(){
-        recipesLoop.push($(this).find('.info .top h2 a').attr('href'));
-      })
-      recipesources = recipesources.concat(recipesLoop);
-      recipesScraped += visibleRecipeCards.length;
-
-
-    
-
-
-
-    
-
-
-    return recipesources;
-
-
-
-
-    // recipesData = recipesData.concat(scrapeRecipes(recipesScraped, visibleRecipeCards));
-
-  // function scrapeRecipes(startIndex, visibleRecipeCards){
-  //   let recipesData = [];
-  //   $(visibleRecipeCards).each(function(){
-  //     let recipe = {};
-  //     recipe.source = $(this).find('.info .top h2 a').attr('href');
-  //     recipesData.push(recipe);
-  //   })
-  //   return recipesData;
-  // }
-
-  })
-  .then(function (urls) {
-    let recipelist = [];
-    for (let i = 0; i < urls.length; i++) {
-      nightmare.goto('https://www.koket.se'+urls[i])
-      .wait(5000)
-      .evaluate(function () {
-        let recipe = {};
-        recipe.title = $(this).find('.recipe-content-wrapper h1').text();
-        recipelist.push(recipe);
-
-      })
-      .then(function(recipes){
-        console.log(recipes);
-      })
-    }
-
-
-
-    console.log(recipelist);
-  })
-  .catch(function (error) {
-    console.error('Search failed:', error);
-  });
-
-
+});
